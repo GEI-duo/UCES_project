f2e5f8b74e19738c40bdec725489420c23263bb1
drownedd <drwn.it@gmail.com>
2024-10-18 18:26:07 +0100

    Hello world LED

--- meeple/src/main.cpp
+++ meeple/src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PIN_LED 3
+#define PIN_HALL_SENSOR 5
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_HALL_SENSOR, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.write("Lights out!");
+
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
 }
